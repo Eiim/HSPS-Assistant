92ae70b1bd4b90f691e15a64de9f83583b238208
Ethan Chapman <ethan@we3chapmans.com>
2023-05-02 18:52:49 +0200

    Check version

--- src/main/java/io/github/eiim/hspsassistant/TestHud.java
+++ src/main/java/io/github/eiim/hspsassistant/TestHud.java
@@ -2,12 +2,14 @@ package io.github.eiim.hspsassistant;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.maven.artifact.versioning.ArtifactVersion;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.util.InputMappings;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 
 public class TestHud {
@@ -23,11 +25,15 @@ public class TestHud {
 	private static int jump;
 	private static int sprint;
 	
+	//private static String mcVersion;
+	private static ArtifactVersion mcVersion;
+	private static boolean isNew;
+	
 	@SubscribeEvent
     public void onRender(RenderGameOverlayEvent.Text event) {
 		fr.func_238405_a_(event.getMatrixStack(), "Hypixel Server Parkour", 10, 10, 0xFFFFFF);
 		fr.func_238405_a_(event.getMatrixStack(), "Lobby Map", 10, 20, 0xFFFFFF);
-		fr.func_238405_a_(event.getMatrixStack(), "All Checkpoints 1.8-1.12", 10, 30, 0xFFFFFF);
+		fr.func_238405_a_(event.getMatrixStack(), "All Checkpoints "+(isNew ? "1.13+" : "1.8-1.12"), 10, 30, 0xFFFFFF);
 		
 		boolean forwardDown = InputMappings.isKeyDown(mchandle, forward);
 		boolean leftDown = InputMappings.isKeyDown(mchandle, left);
@@ -55,6 +61,9 @@ public class TestHud {
 		back = mc.gameSettings.keyBindBack.getKey().getKeyCode();
 		jump = mc.gameSettings.keyBindJump.getKey().getKeyCode();
 		sprint = mc.gameSettings.keyBindSprint.getKey().getKeyCode();
+		
+		mcVersion = ModList.get().getModFileById("minecraft").getMods().get(0).getVersion();
+		isNew = mcVersion.getMinorVersion() > 12;
 	}
 	
 	private static final Logger LOGGER = LogManager.getLogger();
